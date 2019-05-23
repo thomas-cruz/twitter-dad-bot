e564e5e8b8382961bbd5e9235ec42e1994ee5026
thomas-cruz <tom.ortega47@gmail.com>
2019-05-23 14:43:54 +0800

    Added dadbot script, basic fuctionality

---
+++ dadbot.py
@@ -0,0 +1,47 @@
+import time
+import tweepy
+import requests
+import json
+import re
+
+
+
+CONSUMER_KEY = 'YJyVqt1sZMW0QMSJgUY0kZkA1'
+CONSUMER_SECRET = 'KUgX9jaKp4A1yHsW2NypkRLWkrK2UOcaBlLAq0byysnvfCQNUT'
+ACCESS_KEY = '1131424449234759680-jBgck1ao1VmVdMXdp0AuDaetVscGhs'
+ACCESS_SECRET = '9HzbXowzPV2zWtb9DyPh8pHMElqst9AiZcwStFXbZimdC'
+
+
+INTERVAL = 3600 #tweets every hour
+#INTERVAL = 15  # every 15 seconds, for testing
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+while True:
+	search = 'I’m'
+
+	for tweet in tweepy.Cursor(api.search,q = "'I’m -filter:retweets lang:en",exclude_replies = True, tweet_mode='extended').items():
+		print('FULL TWEET '+tweet.full_text)
+		try:
+			phrase = re.split("I’m | I’M | i’m | im | Im | IM | I'm | I'M | i'm",api.get_status(tweet._json['id_str'], tweet_mode='extended')._json['full_text'])[1]
+			phrase = 'Hi '+ phrase + ", I’m Dad"
+			print("Final phrase: "+phrase)
+		except:
+			break
+
+		try:
+			tweetId = tweet.id
+			username = tweet.user.screen_name
+			api.update_status("@" + username + " " + phrase, in_reply_to_status_id = tweetId)
+			print ("Replied with " + phrase)
+
+		except tweepy.TweepError as e:
+			print(e.reason)
+
+		except StopIteration:
+			break
+
+		time.sleep(INTERVAL)
+
+
