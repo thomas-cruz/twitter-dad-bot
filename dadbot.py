d110bb90d2318bbd0a8e4df8e80b63ef9f7c72f2
thomas-cruz <50308862+thomas-cruz@users.noreply.github.com>
2019-05-24 11:04:12 +0800

    I just went through encoding hell code is so bad
    Tried for basically 8 hours to get around ’ and '

--- dadbot.py
+++ dadbot.py
@@ -1,8 +1,7 @@
+# coding: utf-8
 import time
 import tweepy
-import requests
-import json
-import re
+import unidecode
 
 
 
@@ -13,32 +12,44 @@ ACCESS_SECRET = ''
 
 
 INTERVAL = 3600 #tweets every hour
-#INTERVAL = 15  # every 15 seconds, for testing
+#INTERVAL = 120  # every 15 seconds, for testing
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+a = ["I’m", "i’m",  "im",  "Im",  "IM",  "I'm",  "I'M",  "i'm"]
+test = "I'm so glad I don't fuck my followers. 😂 This shit finna get messy. Lmao."
+
 
 while True:
-	for tweet in tweepy.Cursor(api.search,q = "I’m -filter:retweets lang:en",exclude_replies = True, tweet_mode='extended').items():
+	for tweet in tweepy.Cursor(api.search,q = "I'm -filter:retweets lang:en",exclude_replies = True, tweet_mode='extended').items():
 		print('FULL TWEET '+tweet.full_text)
 		try:
-			phrase = tweet.full_text.encode()
-			phrase = re.split("im | Im | IM | I'm | I'M | i'm", phrase)[1]
-			phrase = 'Hi '+ phrase + ", I'm Dad"
-			print("Final phrase: "+phrase)
+		   print("shit")
+		   test = tweet.full_text.replace('\n', ' ').encode('ascii', 'ignore').decode('ascii', 'ignore')
+		   phrase = unidecode.unidecode(test)
+		   print("shitter " + phrase)
+		   if any(s in phrase for s in a):
+		       test = phrase.replace("im", '#').replace("Im", '#').replace("IM", '#').replace("I'm", '#').replace("I'M", '#').replace("i'm", '#').split('#')
+		   else:
+		       phrase = ''
+		   print(test)
+		   test = filter(None, test)
+		   phrase = test[-1]
 		except:
-			break
+		    print("restart me")
+		    sys.exit()
 
-		try:
-			tweetId = tweet.id
-			username = tweet.user.screen_name
-			api.update_status("@" + username + " " + phrase, in_reply_to_status_id = tweetId)
-			print ("Replied with " + phrase)
+		phrase = 'Hi '+ phrase + ", I'm Dad"
+		print("Final phrase: "+phrase)
 
-		except tweepy.TweepError as e:
-			print(e.reason)
-
-		except StopIteration:
-			break
-
-		time.sleep(INTERVAL)
+		if phrase.strip():
+		    try:
+		        tweetId = tweet.id
+		        username = tweet.user.screen_name
+		        api.update_status("@" + username + " " + phrase, in_reply_to_status_id = tweetId)
+		        print ("Replied with " + phrase)
+		        time.sleep(INTERVAL)
+		    except tweepy.TweepError as e:
+		         print(e.reason)
+		    except StopIteration:
+		         break
